2985c5b43a2f2bfea4456d83602414481506c7d9
David Herman <bitspittle@gmail.com>
2023-02-28 21:12:11 -0800

    Add native targets to Truthish

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,39 +25,38 @@ kotlin {
     js(IR) {
         browser()
     }
+
+    linuxX64()
+    macosArm64() // Mac M1
+    macosX64() // Mac Intel
+    mingwX64() // Windows
+
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-common"))
-            }
-        }
+        val commonMain by getting
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
             }
         }
-        val jvmMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-jdk8"))
-            }
-        }
+
+        val jvmMain by getting
         val jvmTest by getting {
             dependencies {
                 implementation(kotlin("test"))
                 implementation(kotlin("test-junit"))
             }
         }
-        val jsMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-js"))
-            }
-        }
+        val jsMain by getting
         val jsTest by getting {
             dependencies {
                 implementation(kotlin("test-js"))
             }
         }
+
+        val linuxX64Main by getting
+        val macosArm64Main by getting
+        val macosX64Main by getting
+        val mingwX64Main by getting
     }
 }
 
