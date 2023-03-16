37d4595c3d011ee0a9e9607ae7324662179a11b8
David Herman <bitspittle@gmail.com>
2023-03-16 16:02:45 -0700

    Add coverage badge workflow and build script support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.dokka.gradle.DokkaTask
+import javax.xml.parsers.DocumentBuilderFactory
 
 plugins {
     kotlin("multiplatform") version "1.8.10"
     id("org.jetbrains.dokka") version "1.7.20"
+    id("org.jetbrains.kotlinx.kover") version "0.6.1"
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin") version "1.2.0"
@@ -16,6 +18,40 @@ repositories {
 
 group = "com.varabyte.truthish"
 version = "0.6.6-SNAPSHOT"
+
+tasks.register("printLineCoverage") {
+    group = "verification" // Put into the same group as the `kover` tasks
+    dependsOn("koverXmlReport")
+    doLast {
+        val report = file("$buildDir/reports/kover/xml/report.xml")
+
+        val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(report)
+        val rootNode = doc.firstChild
+        var childNode = rootNode.firstChild
+
+        var coveragePercent = 0.0
+
+        while (childNode != null) {
+            if (childNode.nodeName == "counter") {
+                val typeAttr = childNode.attributes.getNamedItem("type")
+                if (typeAttr.textContent == "LINE") {
+                    val missedAttr = childNode.attributes.getNamedItem("missed")
+                    val coveredAttr = childNode.attributes.getNamedItem("covered")
+
+                    val missed = missedAttr.textContent.toLong()
+                    val covered = coveredAttr.textContent.toLong()
+
+                    coveragePercent = (covered * 100.0) / (missed + covered)
+
+                    break
+                }
+            }
+            childNode = childNode.nextSibling
+        }
+
+        println("%.1f".format(coveragePercent))
+    }
+}
 
 kotlin {
     jvmToolchain(8) // Used by Android to set JDK version used by kotlin compilation
