72e68aaa64fc02a717a96bbeeb76ec96239f8168
David Herman <bitspittle@gmail.com>
2024-04-12 20:05:46 -0700

    Begin working on v1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 }
 
 group = "com.varabyte.truthish"
-version = "1.0.0"
+version = "1.0.1-SNAPSHOT"
 
 tasks.register("printLineCoverage") {
     group = "verification" // Put into the same group as the `kover` tasks
