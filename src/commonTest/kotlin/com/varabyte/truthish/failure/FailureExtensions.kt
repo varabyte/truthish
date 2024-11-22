4c63fecc5bfab9bcf78bb91948ceb0efb7a0b529
David Herman <bitspittle@gmail.com>
2024-11-22 09:44:34 -0800

    Nit: Add header comments to assertSubstrings

--- src/commonTest/kotlin/com/varabyte/truthish/failure/FailureExtensions.kt
+++ src/commonTest/kotlin/com/varabyte/truthish/failure/FailureExtensions.kt
@@ -1,5 +1,12 @@
 package com.varabyte.truthish.failure
 
+/**
+ * Verify that all of the passed in substrings are present in the final report.
+ *
+ * It can be difficult to test the exact output of a failure report, and it would be fragile to check the content of the
+ * whole string, since text can change over time. However, checking for the presence of certain substrings can be a
+ * reasonable compromise.
+ */
 fun Report.assertSubstrings(vararg substrings: String) {
     val reportStr = this.toString()
     for (substring in substrings) {
@@ -17,6 +24,9 @@ $reportStr
     }
 }
 
+/**
+ * @see [Report.assertSubstrings]
+ */
 internal fun ReportError.assertSubstrings(vararg substrings: String) {
     this.report.assertSubstrings(*substrings)
 }
