edda9264dd53fa45e11b76afc932a8e3b1fab0c3
David Herman <bitspittle@gmail.com>
2025-06-11 16:55:55 -0700

    Migrate publishing to vanniktech plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 @file:OptIn(ExperimentalWasmDsl::class)
 
-import org.jetbrains.dokka.gradle.DokkaTask
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.plugin.LanguageSettingsBuilder
@@ -10,10 +10,8 @@ plugins {
     kotlin("multiplatform") version "2.1.0"
     id("org.jetbrains.dokka") version "1.9.20"
     id("org.jetbrains.kotlinx.kover") version "0.8.3"
-    `maven-publish`
-    signing
-    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
     id("com.android.library") version "8.3.0"
+    id("com.vanniktech.maven.publish") version "0.32.0"
 }
 
 repositories {
@@ -140,144 +138,60 @@ android {
     }
 }
 
-fun shouldSign() = (findProperty("truthish.sign") as? String).toBoolean()
-fun shouldPublishToGCloud(): Boolean {
-    return (findProperty("truthish.gcloud.publish") as? String).toBoolean()
-            && findProperty("gcloud.artifact.registry.secret") != null
-}
-fun shouldPublishToMavenCentral(): Boolean {
-    return (findProperty("truthish.maven.publish") as? String).toBoolean()
-            && findProperty("ossrhToken") != null && findProperty("ossrhTokenPassword") != null
-}
+val gcloudSecret: String? get() = (findProperty("gcloud.artifact.registry.secret") as? String)
 
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+    signAllPublications()
 
-val VARABYTE_REPO_URL = uri("https://us-central1-maven.pkg.dev/varabyte-repos/public")
-fun MavenArtifactRepository.gcloudAuth() {
-    url = VARABYTE_REPO_URL
-    credentials {
-        username = "_json_key_base64"
-        password = findProperty("gcloud.artifact.registry.secret") as String
-    }
-    authentication {
-        create<BasicAuthentication>("basic")
-    }
-}
-
-val SONATYPE_RELEASE_REPO_URL = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-val SONATYPE_SNAPSHOT_REPO_URL = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-fun MavenArtifactRepository.sonatypeAuth() {
-    url = if (!version.toString().endsWith("SNAPSHOT")) SONATYPE_RELEASE_REPO_URL else SONATYPE_SNAPSHOT_REPO_URL
-    credentials {
-        username = findProperty("ossrhToken") as String
-        password = findProperty("ossrhTokenPassword") as String
-    }
-    authentication {
-        create<BasicAuthentication>("basic")
+    pom {
+        val githubPath = "https://github.com/varabyte/truthish"
+        name.set("Truthish")
+        description.set("A Google Truth inspired testing library written in Kotlin supporting multi-platform projects")
+        url.set(githubPath)
+        scm {
+            url.set(githubPath)
+            val connectionPath = "scm:git:${githubPath}.git"
+            connection.set(connectionPath)
+            developerConnection.set(connectionPath)
+        }
+        developers {
+            developer {
+                id.set("bitspittle")
+                name.set("David Herman")
+                email.set("bitspittle@gmail.com")
+                url.set("https://github.com/bitspittle")
+            }
+        }
+        licenses {
+            license {
+                name.set("Apache-2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
     }
 }
-
 
 repositories {
     mavenCentral()
 }
 
-val dokkaHtml by tasks.getting(DokkaTask::class)
-
-val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
-    dependsOn(dokkaHtml)
-    archiveClassifier.set("javadoc")
-    from(dokkaHtml.outputDirectory)
-}
-
 publishing {
     publications {
-        if (shouldSign()) {
-            if (shouldPublishToGCloud()) {
-                repositories {
-                    maven {
-                        name = "GCloudMaven"
-                        gcloudAuth()
+        gcloudSecret?.let { gcloudSecret ->
+            repositories {
+                maven {
+                    name = "GCloudMaven"
+                    url = uri("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+                    credentials {
+                        username = "_json_key_base64"
+                        password = gcloudSecret
+                    }
+                    authentication {
+                        create<BasicAuthentication>("basic")
                     }
                 }
             }
-            if (shouldPublishToMavenCentral()) {
-                repositories {
-                    maven {
-                        name = "SonatypeMaven"
-                        sonatypeAuth()
-                    }
-                }
-            }
-        }
-
-        withType<MavenPublication> {
-            artifact(javadocJar)
-            pom {
-                val githubPath = "https://github.com/varabyte/truthish"
-                name.set("Truthish")
-                description.set("A Google Truth inspired testing library written in Kotlin supporting multi-platform projects")
-                url.set(githubPath)
-                scm {
-                    url.set(githubPath)
-                    val connectionPath = "scm:git:${githubPath}.git"
-                    connection.set(connectionPath)
-                    developerConnection.set(connectionPath)
-                }
-                developers {
-                    developer {
-                        id.set("bitspittle")
-                        name.set("David Herman")
-                        email.set("bitspittle@gmail.com")
-                        url.set("https://github.com/bitspittle")
-                    }
-                }
-                licenses {
-                    license {
-                        name.set("Apache-2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-            }
-        }
-    }
-}
-
-if (shouldSign()) {
-    // Workaround for https://youtrack.jetbrains.com/issue/KT-61858
-    val signingTasks = tasks.withType<Sign>()
-    tasks.withType<AbstractPublishToMaven>().configureEach {
-        mustRunAfter(signingTasks)
-    }
-
-    // If "shouldSign" returns true, then singing password should be set
-    val secretKeyRingExists = (findProperty("signing.secretKeyRingFile") as? String)
-        ?.let { File(it).exists() }
-        ?: false
-
-    // If "shouldSign" returns true, then singing password should be set
-    val signingPassword = findProperty("signing.password") as String
-
-    signing {
-        // If here, we're on a CI. Check for the signing key which must be set in an environment variable.
-        // See also: https://docs.gradle.org/current/userguide/signing_plugin.html#sec:in-memory-keys
-        if (!secretKeyRingExists) {
-            val signingKey: String? by project
-            useInMemoryPgpKeys(signingKey, signingPassword)
-        }
-
-        // Signing requires following steps at https://docs.gradle.org/current/userguide/signing_plugin.html#sec:signatory_credentials
-        // and adding singatory properties somewhere reachable, e.g. ~/.gradle/gradle.properties
-        sign(publishing.publications)
-    }
-}
-
-nexusPublishing {
-    repositories {
-        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            (findProperty("ossrhToken") as? String)?.let { username.set(it) }
-            (findProperty("ossrhTokenPassword") as? String)?.let { password.set(it) }
         }
     }
 }
